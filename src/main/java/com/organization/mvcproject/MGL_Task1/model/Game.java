11b38bb5b7b702ee366fdc722b645582f55e3ec0
alexander.b.white <alexander.b.white@AFSWW4AERWBVSX.us.accenturefederal.com>
2021-05-12 09:53:02 -0500

    Variable changes

--- src/main/java/com/organization/mvcproject/MGL_Task1/model/Game.java
+++ src/main/java/com/organization/mvcproject/MGL_Task1/model/Game.java
@@ -5,32 +5,32 @@ import org.springframework.stereotype.Component;
 @Component
 public class Game {
 
-	private Long game_id;
-	private String game_name;
-	private String game_genre;
+	Long gameId;
+	String gameName;
+	String gameGenre;
 
-	public Long getGame_id() {
-		return game_id;
+	public Long getId() {
+		return gameId;
 	}
 
-	public void setGame_id(Long game_id) {
-		this.game_id = game_id;
+	public void setId(Long gameId) {
+		this.gameId = gameId;
 	}
 
-	public String getGame_name() {
-		return game_name;
+	public String getName() {
+		return gameName;
 	}
 
-	public void setGame_name(String game_name) {
-		this.game_name = game_name;
+	public void setName(String gameName) {
+		this.gameName = gameName;
 	}
 
-	public String getGame_genre() {
-		return game_genre;
+	public String getGenre() {
+		return gameGenre;
 	}
 
-	public void setGame_genre(String game_genre) {
-		this.game_genre = game_genre;
+	public void setGenre(String gameGenre) {
+		this.gameGenre = gameGenre;
 	}
 
 }
