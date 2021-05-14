f10388df2a2433a1f424f3fa9abbf76f43bf224e
alexander.b.white <alexander.b.white@AFSWW4AERWBVSX.us.accenturefederal.com>
2021-05-14 13:41:16 -0500

    Refactoring Package names to follow java standards

--- src/main/java/com/organization/mvcproject/mgl/controller/MGL_Controller.java
+++ src/main/java/com/organization/mvcproject/mgl/controller/MGL_Controller.java
@@ -1,4 +1,4 @@
-package com.organization.mvcproject.MGL_Task1.controller;
+package com.organization.mvcproject.mgl.controller;
 
 import java.util.List;
 
@@ -13,13 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.organization.mvcproject.MGL_Task1.model.Game;
-import com.organization.mvcproject.MGL_Task1.model.Review;
-import com.organization.mvcproject.MGL_Task1.service.Game_Service;
+import com.organization.mvcproject.mgl.model.Game;
+import com.organization.mvcproject.mgl.model.Review;
+import com.organization.mvcproject.mgl.service.Game_Service;
 
 
 @Controller
-public class MGL_Task1_Controller {
+public class MGL_Controller {
 
 	@Autowired
 	private Game_Service javaGameService;
